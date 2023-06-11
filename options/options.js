3f07fdad2d99c69f59e25aa07ef152e18fe0c864
Crowdbotics <team@crowdbotics.com>
2023-06-11 23:33:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-band-41425",
+  url: "https://wild-band-41425.botics.co",
+  api: "https://wild-band-41425.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
